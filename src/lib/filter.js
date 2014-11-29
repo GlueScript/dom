9106314a0e72cd213b126729805f22a6e3971f74
Tim Rodger <tim.rodger@gmail.com>
2014-11-29 00:08:42 +0000

    First filter expecting an xpath

--- src/lib/filter.js
+++ src/lib/filter.js
@@ -1,9 +1,21 @@
+var xpath = require('xpath'),
+    parser = require('xmldom').DOMParser;
+
 /*
- * extract items from doc (a dom html/xml document) using filter parameter
+ * extract items from input (a dom html/xml document) using filter parameter
  * return an array of 'items' 
  * allow filtering for elements or attributes
  */
-exports.extract = function(doc, filter) {
-    console.log('filter: ' + doc + ' : ' + filter);
-    return [['one thing']];
+exports.extract = function(input, filter) {
+    console.log('filter: ' + input + ' : ' + filter);
+
+    var doc = new parser().parseFromString(input);
+    // construct xpath - assume filter is an xpath
+    var nodes = xpath.select(filter, doc);
+    var items = [];
+    nodes.forEach(function(i){
+        // works for elements
+        items.push(i.firstChild.data); 
+    });
+    return items;
 };
