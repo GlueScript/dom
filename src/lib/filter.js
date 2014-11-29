329d5b690ef1a233c34d3a96b564eedcb3b22945
Tim Rodger <tim.rodger@gmail.com>
2014-11-29 09:59:28 +0000

    Catch errors from invalid xpath strings

--- src/lib/filter.js
+++ src/lib/filter.js
@@ -3,23 +3,28 @@ var xpath = require('xpath'),
 
 /*
  * extract items from input (a dom html/xml document) using filter parameter
- * return an array of 'items' 
+ * return an array of raw items 
  * allow filtering for elements or attributes
  */
 exports.extract = function(input, filter) {
 
-    var doc = new parser().parseFromString(input);
+    var items = [];
 
     // construct xpath - assume filter is an xpath
-    var nodes = xpath.select(filter, doc);
-    var items = [];
-    nodes.forEach(function(i) {
-        // works for elements and attributes
-        if (i.firstChild){
-            items.push(i.firstChild.toString());
-        } else {
-            items.push(i.value);
-        }
-    });
+    try {
+        var doc = new parser().parseFromString(input);
+        var nodes = xpath.select(filter, doc);
+        nodes.forEach(function(i) {
+            // works for elements and attributes
+            if (i.firstChild){
+                items.push(i.firstChild.toString());
+            } else {
+                items.push(i.value);
+            }
+        });
+    } catch (e){
+        console.log(e);
+    }
+
     return items;
 };
