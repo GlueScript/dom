495aed73bdd254bd69c747261d6c5275de41699b
Tim Rodger <tim.rodger@gmail.com>
2014-12-02 08:30:21 +0000

    Updates query param to be xpath not f

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 dom
 ===
 
-Node service to work on DOM documents using xpath. Post your document with the correct Content-Type header and a filter param containing an xpath.
+Node service to work on DOM documents using xpath. Post your document with the correct Content-Type header and a query param containing an xpath.
 
 Usage:
 
-  curl -X POST -d @web-page.html -H 'Content-Type: text/html' 'http://dom.service/?f=//a/@href'
+  curl -X POST -d @web-page.html -H 'Content-Type: text/html' 'http://dom.service/?xpath=//a/@href'
 
 This will respond with a json array of the href attribute elements contained by the document.
--- src/index.js
+++ src/index.js
@@ -5,26 +5,19 @@ var app = require('express')(),
 app.use(bodyParser.text({type : 'text/*', limit: '1024kb'}));
 app.use(bodyParser.text({type : 'application/xml'}));
 
-// use env.PORT if set
-var PORT = 8780;
-
 // App
 app.get('/', function (req, res) {
-    res.json({"description":"DOM - post a document with an xpath filter query (f=//a/@href) to extract the specified items"});
+    res.json({"description":"DOM - post a document with an xpath filter query (xpath=//a/@href) to extract the specified items"});
 });
 
 app.post('/', function(req, res) {
     // validation? let the filter module do that?
-    
-    // get document from request body
-    // get filter from query string
-    // invoke business logic using doc and filter
-    var result = filter.extract(req.body, req.param('f'));
-
-    // respond with json array of 0+ items
-    res.setHeader('Content-Type', 'application/json');
-    res.send(result);
+    var result = filter.extract(req.body, req.param('xpath'));
+    res.json(result);
 });
+
+// use env.PORT if set
+var PORT = process.env.PORT || 8780;
 
 app.listen(PORT);
 
